a0fcdc367a378faac0c2b5109932b6394e8c91f3
jerome greder <j.greder@staro.ch>
2024-05-04 23:02:48 +0200

    Refactor slideshow functionality and CSS structure
    The slideshow functionality has been simplified and improved. Code has been refactored to make it more concise and efficient. In addition, the CSS file has been restructured, it has transitioned to SCSS for better maintenance, readability, and reusability. The stylesheet is now organized into modules, with a new file created for color variables. Improvements were also made to responsive designs for different screens.

--- script/script.js
+++ script/script.js
@@ -1,44 +1,30 @@
-// positions the current slide
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// used to control the next an previous buttons, by adding or subtracting the number of slides
+// Set a timer to change slide every 3 seconds
+setInterval(nextSlide, 6000, 1);
+
 function nextSlide(n) {
   showSlides(slideIndex += n);
 }
 
-
-//slideshow function, it makes this whole thing work
 function showSlides(n) {
-  let i;
   let slides = document.getElementsByClassName("slide");
   let dots = document.getElementsByClassName("dot");
 
-  //makes sure the slide show dosent end
-  if (n > slides.length) {
+  slideIndex = n > slides.length ? 1 : n < 1 ? slides.length : slideIndex;
 
-    slideIndex = 1;
-  }
-  if (n < 1) {
+  Array.from(slides).forEach((slide, i) => {
+    slide.style.display = "none";
+    if (i === slideIndex - 1) {
+      slide.style.display = "block";
+    }
+  });
 
-    // "counts" the present slides and puts it inside the value "slideIndex"
-    slideIndex = slides.length;
-  }
-
-
-  // puts the other images to display none
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-
-  //replaces the class name "active" with nothing who in return nullifying the hover effet in css
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-
-  //shows the current image with the display "active"
-  slides[slideIndex-1].style.display = "block";
-
-  //links the current "dot" with the class name "active" who in return triggers a hover effect in css
-  dots[slideIndex-1].className += " active";
+  Array.from(dots).forEach((dot, i) => {
+    dot.className = dot.className.replace(" active", "");
+    if (i === slideIndex - 1) {
+      dot.className += " active";
+    }
+  });
 }
